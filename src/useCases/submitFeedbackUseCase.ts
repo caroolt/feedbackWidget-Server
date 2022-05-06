c94dea7a12a022c048559af74fd4d9eeec56ea35
carolinatc1405 <carolinatc1405@gmail.com>
2022-05-06 15:35:35 -0300

    erro de digitação

--- src/useCases/submitFeedbackUseCase.ts
+++ src/useCases/submitFeedbackUseCase.ts
@@ -23,7 +23,7 @@ export class SubmitFeedbackUseCase {
       throw new Error('Comment is required');
     }
 
-    if (screenshot && !screenshot.startsWith('data:image/png;bas64')) {
+    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
       throw new Error('Invalid screenshot format.');
     }
 
